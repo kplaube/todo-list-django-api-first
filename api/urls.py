bbe62742594e8c5c569b5a89567938fa715405c0
Klaus Laube <klaus.laube@tradeshift.com>
2020-03-09 11:53:13 +0100

    Fifth step: Document the API for release

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,22 @@
 from django.urls import include, path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from rest_framework.routers import DefaultRouter
 
 from . import views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TODO List API",
+        default_version="v1",
+        description="DjangoDay project :D",
+        public=True,
+    ))
 
 router = DefaultRouter()
 router.register(r"tasks", views.TasksViewSet)
 
 urlpatterns = [
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0)),
     path('', include(router.urls)),
 ]
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
     # 3rd-party
     'rest_framework',
+    'drf_yasg',
 
     # Local
     'api',
